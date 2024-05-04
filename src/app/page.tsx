9079e765965ed09f202f0290a8f265aee8e6371c
enriquegiottonini <enriquegiottonini@gmail.com>
2024-05-04 16:02:52 -0700

    feat: fetching proyects from api.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import { HeroSection } from "@/components/ui/home/hero-section";
 import { UpcomingEventsSection } from "@/components/ui/home/events-section";
 import { SoyLCCSection } from "@/components/ui/home/soy-lcc-section";
-import { GallerySection } from "@/components/ui/home/gallery-section";
 import { JobBoardSection } from "@/components/ui/home/job-board-section";
 import { getEventsDates, getEventsAfterDate } from "@/lib/api/events";
 import { getAllSoyLCCVideos } from "@/lib/api/soy-lcc-videos";
+import { getAllProyectOfferings } from "@/lib/api/proyect-offerings";
+import { ProyectOffering } from "@/lib/types";
 
 export default async function HomePage() {
   const allEventsDates = await getEventsDates();
   const upcomingEvents = await getEventsAfterDate(new Date());
   const soyLCCVideos = await getAllSoyLCCVideos();
+  const proyectOfferings: ProyectOffering[] = await getAllProyectOfferings();
 
   return (
     <div>
@@ -19,8 +21,7 @@ export default async function HomePage() {
         upcomingEvents={upcomingEvents}
       />
       <SoyLCCSection soyLCCVideos={soyLCCVideos} />
-      <JobBoardSection />
-      {/* <GallerySection /> */}
+      <JobBoardSection proyectOfferings={proyectOfferings}/>
     </div>
   );
 }
