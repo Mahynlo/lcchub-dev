e63bf57626b83d8ff755d94200d9dbf048ec09c3
enriquegiottonini <enriquegiottonini@gmail.com>
2024-05-15 22:42:26 -0700

    fix

--- src/app/dashboard/auth/profile/lccmap/page.tsx
+++ src/app/dashboard/auth/profile/lccmap/page.tsx
@@ -5,12 +5,13 @@ import { StudentInfoContext } from "../layout";
 import { useContext, useEffect, useState } from "react";
 import { getCurriculumMaps, cacheSubjectInfo } from "@/lib/api/curriculumMap-by-key";
 import { AxisCard } from "@/components/ui/dashboard/lccmap/axiscard";
+import { cn } from "@/lib/utils";
 
 export default function Page() {
   const student = useContext(StudentInfoContext);
   const key = student?.studyPlan;
   const [curriculumMap, setCurriculumMap] = useState<CurriculumMap | null>(null);
-  const [cacheSubject, setCacheSubject] = useState<Map<string, Subject> | null>(null);
+  const [cacheSubject, setCacheSubject] = useState<Map<string, Subject>>(new Map());
   useEffect(() => {
     if (key) {
       getCurriculumMaps(key).then((curriculumMap) => {
@@ -35,13 +36,58 @@ export default function Page() {
           <p className="text-4xl font-bold">{curriculumMap.totalCredits}</p>
         </div>
         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-          <AxisCard title="Básico" credits={curriculumMap.basicCredits} color="#ffff66" />
-          <AxisCard title="Común" credits={curriculumMap.commonCredits} color="#e8eef7" />
+          <AxisCard title="Básico" credits={curriculumMap.basicCredits} color="#e8eef7" />
+          <AxisCard title="Común" credits={curriculumMap.commonCredits} color="#ffff66" />
           <AxisCard title="Profesionalizante" credits={curriculumMap.electiveCredits} color="#ff9966" />
           <AxisCard title="Especializante" credits={curriculumMap.specialistCredits} color="#99ff66" />
           <AxisCard title="Integrador" credits={curriculumMap.integratorCredits} color="#9966ff" />
         </div>
       </div>
+      <div className="container grid gap-8 px-4 py-10 md:px-6">
+        <CurriculumMapSection semesters={curriculumMap.semesters} subjectCache={cacheSubject} />
+        </div>
     </div>
   );
+}
+
+
+function CurriculumMapSection( { semesters, subjectCache } : { semesters: string[], subjectCache: Map<string, Subject> }) {
+  return (
+    <div className={cn(`grid grid-cols-8 gap-1`)}>
+      {semesters.map((semester, i) => (
+        <SemesterCard key={i} semester={semester} subjectCache={subjectCache} />
+      ))}
+    </div>
+  );
+}
+
+function SemesterCard({ semester, subjectCache }: { semester: string, subjectCache: Map<string, Subject> }) {
+  const subjectKeys = semester.split("-");
+  return (
+    <div>
+      <div className={cn(`grid gap-5`)}>
+        {subjectKeys.map((subjectKey, i) => (
+          <SubjectCard key={i} subject={subjectCache.get(subjectKey)} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function SubjectCard({ subject }: { subject: Subject | undefined }) {
+  return (
+    <div className={cn(`flex flex-col bg-white hover:scale-125 p-4 max-h-16 min-h-14 max-w-30 min-w-31 flex items-center justify-center gap-1 ${axisColor(subject?.branch.trim())}`)}>
+      <h3 className="text-[8px] text-center">{subject?.subjectName.substring(0, 25)}</h3>
+      <p className="text-[8px] text-center">{subject?.credits}</p>
+    </div>
+  );
+}
+
+function axisColor(axis: string | undefined) {
+  if (axis == "Basico") return "bg-[#e8eef7]";
+  if (axis == "Comun") return "bg-[#ffff66]";
+  if (axis == "Profesional") return "bg-[#ff9966]";
+  if (axis == "Especializante" || axis == "Selectiva") return "bg-[#99ff66]";
+  if (axis == "Integrador") return "bg-[#9966ff]";
+  return "bg-[#e8ef7]";
 }
--- src/lib/api/curriculumMap-by-key.ts
+++ src/lib/api/curriculumMap-by-key.ts
@@ -16,59 +16,7 @@ export async function getCurriculumMaps(key: string) {
         return null;
     }
   }
-// async function getSubjectInfo(subjectKey: string) {
-//     const docRef = doc(db, "subjects", subjectKey);
-//     const docSnap = await getDoc(docRef);
-//     console.log(docSnap);
-//     if (docSnap.exists()) {
-//         const subject = docSnap.data() as Subject;
-//         return subject;
-//     } else {
-//         return null;
-//     }
-// }
-//cache subject info
-// unstable_cache
 
-// unstable_cache allows you to cache the results of expensive operations, like database queries, and reuse them across multiple requests.
-
-// import { getUser } from './data';
-// import { unstable_cache } from 'next/cache';
- 
-// const getCachedUser = unstable_cache(
-//   async (id) => getUser(id),
-//   ['my-app-user']
-// );
- 
-// export default async function Component({ userID }) {
-//   const user = await getCachedUser(userID);
-//   ...
-// }
-
-//     Good to know:
-
-//         Accessing dynamic data sources such as headers or cookies inside a cache scope is not supported. If you need this data inside a cached function use headers outside of the cached function and pass the required dynamic data in as an argument.
-//         This API uses Next.js' built-in Data Cache to persist the result across requests and deployments.
-
-//     Warning: This API is unstable and may change in the future. We will provide migration documentation and codemods, if needed, as this API stabilizes.
-
-// Parameters
-
-// const data = unstable_cache(fetchData, keyParts, options)()
-
-//     fetchData: This is an asynchronous function that fetches the data you want to cache. It must be a function that returns a Promise.
-//     keyParts: This is an array that identifies the cached key. It must contain globally unique values that together identify the key of the data being cached. The cache key also includes the arguments passed to the function.
-//     options: This is an object that controls how the cache behaves. It can contain the following properties:
-//         tags: An array of tags that can be used to control cache invalidation.
-//         revalidate: The number of seconds after which the cache should be revalidated. Omit or pass false to cache indefinitely or until matching revalidateTag() or revalidatePath() methods are called.
-
-// Returns
-
-// unstable_cache returns a function that when invoked, returns a Promise that resolves to the cached data. If the data is not in the cache, the provided function will be invoked, and its result will be cached and returned.
-
-// cache getSubjectInfo, individual subjects
-
-//  incrementalCache missing
 const getSubjectInfo = unstable_cache(
     async (subjectKey: string) => {
         const docRef = doc(db, "subjects", subjectKey);
@@ -86,7 +34,6 @@ const getSubjectInfo = unstable_cache(
 
 export async function cacheSubjectInfo(program: string[]) {
     const cache = new Map<string, Subject>();
-    console.log("PROGRAM", program);
     for (const semester of program) {
         const subjectKeys = semester.split("-");
         for (const subjectKey of subjectKeys) {
@@ -98,6 +45,5 @@ export async function cacheSubjectInfo(program: string[]) {
             }
         }
     }
-    console.log("CACHE", cache)
     return cache;
 }
