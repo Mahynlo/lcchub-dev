2df0a65ccdd9cf969999327cf07802b734feb702
enriquegiottonini <enriquegiottonini@gmail.com>
2024-05-20 19:37:45 -0700

    fix: prettier in pcmap

--- src/app/dashboard/auth/profile/lccmap/page.tsx
+++ src/app/dashboard/auth/profile/lccmap/page.tsx
@@ -6,6 +6,14 @@ import { useContext, useEffect, useState } from "react";
 import { getCurriculumMaps, cacheSubjectInfo } from "@/lib/api/curriculumMap-by-key";
 import { AxisCard } from "@/components/ui/dashboard/lccmap/axiscard";
 import { cn } from "@/lib/utils";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function Page() {
   const student = useContext(StudentInfoContext);
@@ -26,7 +34,7 @@ export default function Page() {
     }
   }, [key]);
   return curriculumMap && (
-    <div className="w-full">
+    <div className="w-full items-center">
       <div className="container grid gap-8 px-4 md:px-6">
         <div className="grid gap-4">
           <p className="max-w-[700px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
@@ -43,7 +51,8 @@ export default function Page() {
           <AxisCard title="Integrador" credits={curriculumMap.integratorCredits} color="#9966ff" />
         </div>
       </div>
-      <div className="container grid gap-8 px-4 py-10 md:px-6">
+      <h2 className="text-xl font-bold py-6">Mapa curricular</h2>
+      <div className="md:grid md:grid-cols-8 md:gap-10 md:w-full md:h-[800px]">
         <CurriculumMapSection semesters={curriculumMap.semesters} subjectCache={cacheSubject} />
         </div>
     </div>
@@ -53,33 +62,45 @@ export default function Page() {
 
 function CurriculumMapSection( { semesters, subjectCache } : { semesters: string[], subjectCache: Map<string, Subject> }) {
   return (
-    <div className={cn(`grid grid-cols-8 gap-1`)}>
+    <>
       {semesters.map((semester, i) => (
+        <div key={i} className={cn(`grid grid-rows-8 gap-2`)}>
+          <div className="bg-white border-2 rounded-full absolute">
+            <h4 className="">{int2roman(i + 1)}</h4>
+            </div>
         <SemesterCard key={i} semester={semester} subjectCache={subjectCache} />
+        </div>
       ))}
-    </div>
+    </>
   );
 }
 
 function SemesterCard({ semester, subjectCache }: { semester: string, subjectCache: Map<string, Subject> }) {
   const subjectKeys = semester.split("-");
   return (
-    <div>
-      <div className={cn(`grid gap-5`)}>
+    <>
         {subjectKeys.map((subjectKey, i) => (
           <SubjectCard key={i} subject={subjectCache.get(subjectKey)} />
         ))}
-      </div>
-    </div>
+    </>
   );
 }
 
 function SubjectCard({ subject }: { subject: Subject | undefined }) {
   return (
-    <div className={cn(`flex flex-col bg-white hover:scale-125 p-4 max-h-16 min-h-14 max-w-30 min-w-31 flex items-center justify-center gap-1 ${axisColor(subject?.branch.trim())}`)}>
-      <h3 className="text-[8px] text-center">{subject?.abbr || subject?.subjectName}</h3>
-      <p className="text-[8px] text-center">{subject?.credits}</p>
-    </div>
+      <Card className={cn(`w-[125px] h-[100px] text-center justify-between ${axisColor(subject?.branch)}`)}>
+        <CardHeader>
+          <CardTitle>
+            <h3 className="text-[12px]">{subject?.abbr || subject?.subjectName.trim()}</h3>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <CardDescription>
+            <p>{subject?.credits}</p>
+          </CardDescription>
+        </CardContent>
+        <CardFooter />
+      </Card>
   );
 }
 
@@ -90,4 +111,14 @@ function axisColor(axis: string | undefined) {
   if (axis == "Especializante" || axis == "Selectiva") return "bg-[#99ff66]";
   if (axis == "Integrador") return "bg-[#9966ff]";
   return "bg-[#e8ef7]";
+}
+
+function int2roman(num: number) {
+  if (typeof num !== "number") return false;
+  var digits = String(+num).split(""),
+    key = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM", "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+    roman = "",
+    i = 3;
+  while (i--) roman = (key[+digits.pop() + i * 10] || "") + roman;
+  return Array(+digits.join("") + 1).join("M") + roman;
 }
