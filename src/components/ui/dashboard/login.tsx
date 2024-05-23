3a9942d50e17eb34e4632d07d95bf97ba1f02350
Enrique Giottonini <enrique.giottonini@ENCEDCHMOLT0807.local>
2024-05-23 10:17:50 -0700

    fix wip

--- src/components/ui/dashboard/login.tsx
+++ src/components/ui/dashboard/login.tsx
@@ -4,10 +4,13 @@ import { Button } from "@/components/ui/button";
 import { Sticker } from "lucide-react";
 import { KeyRound } from "lucide-react";
 import { loginRequest } from "@/lib/msal-config";
-import { useMsal } from "@azure/msal-react";
+import { useMsal, useIsAuthenticated } from "@azure/msal-react";
+import { InteractionStatus } from "@azure/msal-browser";
 
 export default function Login() {
-  const { instance } = useMsal();
+  const { instance, inProgress } = useMsal();
+  const isAuthenticated = useIsAuthenticated();
+
   return (
     <div className="flex min-h-[100dvh] flex-col items-center justify-center bg-gray-100 px-2 dark:bg-gray-900">
       <div className="mx-auto w-full max-w-md space-y-6">
@@ -25,9 +28,9 @@ export default function Login() {
           className="flex w-full items-center justify-center gap-2 rounded-md border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 shadow-sm transition-colors hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 dark:border-gray-800 dark:bg-gray-950 dark:text-gray-50 dark:hover:bg-gray-800 dark:focus:ring-gray-300"
           variant="outline"
           onClick={() => {
-            instance.loginPopup(loginRequest).catch((e) => {
-              console.error(e);
-            });
+            if (!isAuthenticated && inProgress === InteractionStatus.None) {
+              instance.loginPopup(loginRequest);
+            }
           }}
         >
           <KeyRound className="h-5 w-5" />
