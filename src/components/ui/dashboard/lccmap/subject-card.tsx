607537bf9205a814bccca52d3680279633e62e54
Enrique Giottonini <enrique.giottonini@ENCEDCHMOLT0807.local>
2024-06-29 16:29:29 -0700

    feature: modal for all except axis-subjexts.

--- src/components/ui/dashboard/lccmap/subject-card.tsx
+++ src/components/ui/dashboard/lccmap/subject-card.tsx
@@ -2,11 +2,19 @@ import { cn } from "@/lib/utils";
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 import { useContext, useState } from "react";
 import { SubjectShowContext } from "@/lib/types";
@@ -41,27 +49,73 @@ export default function SubjectCard({
   }
 
   return (
-    <Card
-      className={cn(`w-[125px] h-[100px] text-center justify-between 
+    <Dialog>
+      <Card
+        className={cn(`w-[125px] h-[100px] text-center justify-between 
         ${axisColor(subject?.branch)} hover:scale-110
         ${showAll || (subject && showSubject.get(subject.subjectKey)) ? "opacity-100" : "opacity-20"}
         hover:opacity-100
         `)}
-      onClick={subjectClick}
-      onMouseLeave={subjectLeave}
-    >
-      <CardHeader>
-        <CardTitle>
+        onClick={subjectClick}
+        onMouseLeave={subjectLeave}
+      >
+        <CardHeader>
           {subject && (
-            <h3 className="text-[12px]">
-              {subject?.abbr || subject?.subjectName.trim()}
-            </h3>
+            <CardTitle>
+              {isNaN(+subject?.subjectKey) ? (
+                <h3 className="text-[12px]">
+                  {subject?.abbr || subject?.subjectName.trim()}
+                </h3>
+              ) : (
+                <DialogTrigger asChild>
+                  <button>
+                    <h3 className="text-[12px]">
+                      {subject?.abbr || subject?.subjectName.trim()}
+                    </h3>
+                  </button>
+                </DialogTrigger>
+              )}
+            </CardTitle>
           )}
-        </CardTitle>
-      </CardHeader>
-      <CardContent></CardContent>
-      <CardFooter />
-    </Card>
+        </CardHeader>
+        <CardContent></CardContent>
+        <CardFooter></CardFooter>
+      </Card>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            {subject?.subjectKey} - {subject?.subjectName.trim()}
+          </DialogTitle>
+        </DialogHeader>
+        <DialogDescription>{subject?.credits} créditos</DialogDescription>
+        <div>
+          <h1 className="text-sm font-bold">{subject?.academicDivision}</h1>
+          <p className="text-sm">Departamento de {subject?.department}</p>
+          <p className="text-sm">Eje {subject?.branch}</p>
+
+          <div className="grid grid-cols-3 gap-2 mt-2">
+            <p className="text-sm">Teoría: {subject?.theoryHours}</p>
+            <p className="text-sm">Laboratorio: {subject?.labHours}</p>
+            <p className="text-sm">Taller: {subject?.workshopHours}</p>
+          </div>
+
+          <div className="grid grid-cols-3 gap-2 mt-2">
+            <p className="text-sm">Requisitos: {subject?.requirements}</p>
+            <p className="text-sm">Liberaciones: {subject?.releases}</p>
+          </div>
+
+          <div>
+            <h1 className="text-sm font-bold">Requisitos</h1>
+            {subject?.requirements?.split("-").map((req) => (
+              <p className="text-sm" key={req}>
+                {req}
+              </p>
+            ))}
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 }
 
